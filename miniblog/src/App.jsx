5c192ecce35be713604a7678d888f201f707c071
Diego <diegoramos3709@gmail.com>
2024-06-06 14:10:05 -0300

    Initial commit

---
+++ miniblog/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='container'>
+       <h1>Mini blo</h1>
+      </div>
+     
+
+
+    </>
+  )
+}
+
+export default App
