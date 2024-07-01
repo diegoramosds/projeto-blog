995b896bb5a482a677694271de8b1f997a41d312
Diego <diegoramos3709@gmail.com>
2024-07-01 16:37:36 -0300

    criação individual da página de posts

--- miniblog/src/App.jsx
+++ miniblog/src/App.jsx
@@ -18,6 +18,7 @@ import { AuthProvider } from './context/AuthContext'
 import CreatePost from './pages/CreatePost/CreatePost';
 import Dashboard from './pages/Dashboard/Dashboard';
 import Search from './pages/Search/Search';
+import Post from './pages/Post/Post';
 
 function App() {
 
@@ -48,6 +49,7 @@ function App() {
             <Route path='/' element={<Home />}/>
             <Route path='/about' element={<About />}/>
             <Route path='/search' element={<Search />}/>
+            <Route path='/posts/:id' element={<Post />}/>
             <Route path='/login' element={!user ? <Login/> : <Navigate to={"/"}/>}/>
             <Route path='/register' element={!user ? <Register/> : <Navigate to={"/"}/>}/>
             <Route path='/posts/create' element={user ? <CreatePost/> : <Navigate to={"/login"}/>}/>
--- miniblog/src/pages/Register/Register.jsx
+++ miniblog/src/pages/Register/Register.jsx
@@ -1,6 +1,6 @@
 
 import { useAuthentication } from "../../hooks/useAuthentication";
-import styles from "./Register.module.css"
+import styles from './Register.module.css'
 
 import { useState, useEffect } from "react"
 
