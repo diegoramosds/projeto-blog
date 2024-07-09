6025519cc157cf1d69342250bd52fac80ce02885
Diego <diegoramos3709@gmail.com>
2024-07-09 17:08:10 -0300

    funções de delete e edit

---
+++ miniblog/src/pages/EditPost/EditPost.jsx
@@ -0,0 +1,128 @@
+import styles from "./EditPost.module.css";
+
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useAuthValue } from "../../context/AuthContext";
+import { useUpdateDocument } from "../../hooks/useUpdateDocument";
+import { useFetchDocument } from "../../hooks/useFetchDocument";
+
+const EditPost = () => {
+  const {id} = useParams();
+  const { document : post }  = useFetchDocument("posts", id);
+
+  useEffect(() => {
+    if (post) {
+      setTitle(post.title);
+      setImage(post.image);
+      setBody(post.body);
+      
+      const texTags = post.tagsArray.join(", ")
+      setTags(texTags);
+    }
+}, [post]);
+
+const [title, setTitle] = useState("");
+const [image, setImage] = useState("");
+const [body, setBody] = useState("");
+const [tags, setTags] = useState([]);
+const [formError, setFormError] = useState("");
+
+const {user} = useAuthValue();
+
+const {updateDocument, response} = useUpdateDocument("posts");
+
+const navigate = useNavigate();
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  setFormError("")
+
+  // validate image url
+  try {
+    new URL(image)
+  } catch (error) {
+    setFormError("A imagem precisa ser uma URL");
+  }
+
+
+  // criar array de tags
+  const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
+
+  //checar os valores
+  if(!title || !image || !body || !tags) {
+    setFormError("Preencha todos os campos");
+  }
+
+  const data = {
+    title,
+    image,
+    body,
+    tagsArray,
+    uid: user.uid,
+    createdBy: user.displayName,
+  }
+
+  updateDocument(id, data);
+
+  //redirect to home
+  navigate("/dashboard");
+}
+
+  return (
+    <div className={styles.edit_post}>
+        {post && (
+          <>
+          <h2>Editar Post: {post.title}</h2>
+        <p>Faça alterações ds dados do post</p>
+        <form onSubmit={handleSubmit}>
+          <label>
+            <span>Titulo:</span>
+            <input type="text" required 
+            placeholder="Pense em um titulo criativo..."
+            name="title"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title} />
+          </label>
+          <label>
+            <span>Url da imagem:</span>
+            <input type="text" required 
+            placeholder="Insira uma imagem que representa o post"
+            name="image"
+            onChange={(e) => setImage(e.target.value)}
+            value={image} />
+          </label>
+          <p className={styles.preview_title}>Preview da imagem atual:</p>
+          <img className={styles.image_preview} src={post.image} alt={post.title} />
+          <label>
+            <span>Conteúdo:</span>
+            <input type="text" required 
+            placeholder="Insira o conteúdo do post"
+            name="image"
+            onChange={(e) => setBody(e.target.value)}
+            value={body} />
+          </label>
+          <label>
+            <span>Tags:</span>
+            <input type="text" required 
+            placeholder="Insira as tags separadas por vírgulas"
+            name="image"
+            onChange={(e) => setTags(e.target.value)}
+            value={tags} />
+          </label>
+
+          {!response.loading &&  <button className="btn">Editar</button>}
+          {response.loading &&  <button className="btn" disabled>Aguarde</button>}
+
+          {response.error && <p className="error">{response.error}</p>}
+
+          {formError && <p className="error">{formError}</p>}
+         
+        </form>
+        </>
+        )}
+        
+    </div>
+  )
+}
+
+export default EditPost
--- miniblog/src/pages/Post/Post.jsx
+++ miniblog/src/pages/Post/Post.jsx
@@ -2,12 +2,27 @@ import styles from './Post.module.css'
 
 //hooks
 import { useParams } from 'react-router-dom'
+import { useFetchDocument } from '../../hooks/useFetchDocument';
+
 
 const Post = () => {
     const { id } = useParams();
+    const {document : post} = useFetchDocument("posts", id);
   return (
-    <div>
-        <h1>Post {id}</h1>
+    <div className={styles.post_container}>
+        {post && (
+          <>
+          <h1>{post.title}</h1>
+          <img src={post.image} alt={post.title} />
+          <p>{post.body}</p>
+          <h3>Esse post é sobre:</h3>
+          <div className={styles.tags}>
+            {post.tagsArray.map((tag) => (
+             <p key={tag}><span>#</span>{tag}</p>
+            ))}
+          </div>
+          </>
+        )}
     </div>
   )
 }
