1c056f586ef4489c1a90ee3c1b0b3b0d7270a96f
Diego <diegoramos3709@gmail.com>
2024-07-20 14:13:41 -0300

    detalhes do post

--- miniblog/src/pages/Dashboard/Dashboard.jsx
+++ miniblog/src/pages/Dashboard/Dashboard.jsx
@@ -37,9 +37,9 @@ const Dashboard = () => {
            {posts && posts.map((post) => <div key={post.id} className={styles.post_row}>
            <p>{post.title}</p>
            <div>
-            <Link to={`/posts/${post.id}`} className="btn btn-outline">Ver</Link>
-            <Link to={`/posts/edit/${post.id}`} className="btn btn-outline">Editar</Link>
-            <button onClick={() => deleteDocument(post.id)} className="btn btn-outline btn-danger">Excluir</button>
+            <Link to={`/posts/${post.id}`} className="dashboard btn btn-outline">Ver</Link>
+            <Link to={`/posts/edit/${post.id}`} className="dashboard btn btn-outline">Editar</Link>
+            <button onClick={() => deleteDocument(post.id)} className="dashboard btn btn-outline btn-danger">Excluir</button>
            </div>
             </div>)}
          </>
